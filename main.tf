0abd4590b5784946031c02f5501d98c12dd9ae77
ajaykushwah17 <123883271+ajaykushwah17@users.noreply.github.com>
2023-10-19 10:51:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.10.0"
+        }
+        }
+    }
+    provider "aws" {
+        region = "ap-south-1"
+        access_key = "AKIA32BP4XBTFMFY2VFA"
+        secret_key = "ZhUApSJnRyARb9zX5azeN0I5nBvh9khBdNhtRJDT"
+    }
+    resource "aws_instance" "example" {
+  ami           = "ami-067c21fb1979f0b27" 
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "Myec2Instance"
+  }
+    }
